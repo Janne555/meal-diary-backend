5b1e6b1b81a7e76bab58ff057e7e72cb98c49003
Janne Lavila <Janne555@users.noreply.github.com>
2020-06-18 18:20:18 +0300

    change how mongo uri is handled

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-mongoose.connect(`${env.MONGO_URI}/${env.APP_NAME}`)
+mongoose.connect(`${env.MONGO_URI.replace("<dbname>", env.APP_NAME)}`)
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
